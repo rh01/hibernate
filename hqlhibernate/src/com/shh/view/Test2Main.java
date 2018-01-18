1b00172f1e029aa037e5f138f16df24525f27e0f
rh01 <1048157315@qq.com>
2018-01-18 10:43:05 +0800

    update

--- hqlhibernate/src/com/shh/view/Test2Main.java
+++ hqlhibernate/src/com/shh/view/Test2Main.java
@@ -2,6 +2,12 @@ package com.shh.view;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.hql.ast.tree.RestrictableStatement;
+
 import com.shh.util.*;
 
 import com.shh.util.HibernateUtil;
@@ -27,10 +33,26 @@ public class Test2Main {
 		}*/
 		
 		//添加
-		Course c = new Course();
+		/*Course c = new Course();
 		c.setCid(new BigDecimal(4));
 		c.setCname("serclet");
-		HibernateUtil.save(c);
+		HibernateUtil.save(c);*/
+		
+		
+		//所有计算机系的学生年龄加一
+		//查询年龄大于10岁的学生criteria
+		Session s = HibernateUtil.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+		Criteria cri=s.createCriteria(Student.class);
+		//添加检索条件
+		cri.add(Restrictions.gt("sage", new Short((short) 10)));
+		List<Student> list = cri.list();
+		for(Student s1:list){
+			System.out.println(s1.getSname());
+		}
+		
+		tx.commit();
+		
 		
 		
 	}
